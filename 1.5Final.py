f1a51b4d7373046904ced267b7b9d53713127fd9
mysamdog <samburns157@gmail.com>
2016-02-05 13:43:25 -0600

    Update 1.5Final.py

--- 1.5Final.py
+++ 1.5Final.py
@@ -1,8 +1,9 @@
 import wx
 import time
+
 class GameObject(object):
     def __init__(self, x, y, width, height):
-        self.coords, self.velocity, self.dimensions = [[x, y], [x + width, y + height]], [0, 0], [width, height]
+        self.coords, self.dimensions = [[x, y], [x + width, y + height]], [width, height]
     def isTouching(self, compareTo):
         for c in [0, 1]:
             if compareTo.coords[0][0] < self.coords[c][0] < compareTo.coords[1][0] and compareTo.coords[0][1] < self.coords[c][1] < compareTo.coords[1][1]:
@@ -10,43 +11,86 @@ class GameObject(object):
         return False
         
 class Drawable(GameObject):
-    def __init__(self, x, y, width, height, color):
+    def __init__(self, x, y, width, height, color, border = ["#000000", 2]):
         super(Drawable, self).__init__(x, y, width, height)
         self.color = color
+        self.border = border#[color, width] *Temporary
     def draw(self, dc):
-        dc.SetPen(wx.Pen(self.color, 4))
-        dc.DrawRectangle(self.coords[0][0], self.coords[0][1], self.dimensions[0], self.dimensions[1])
+        pen = wx.Pen(self.border[0], self.border[1], wx.SOLID)
+        #print pen.GetJoin()
+        dc.SetPen(pen)#.SetJoin(wx.JOIN_MITER))
+        dc.SetBrush(wx.Brush(self.color, wx.SOLID))
+        dc.DrawRectangle(self.coords[0][0]+self.border[1], self.coords[0][1]+self.border[1], self.dimensions[0], self.dimensions[1])
         
 class Window(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(600, 600))
+        wx.Frame.__init__(self, parent, id, title, size=(1200, 600),  style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
         self.statusbar = self.CreateStatusBar()
         self.statusbar.SetStatusText('0')
         self.board = Board(self)
+        self.SetBackgroundColour("#2A4152")#00F9FF
         self.Centre()
         self.Show(True)
 
 class Board(wx.Panel):
+    TIMER_ID = 42
     def __init__(self, parent):
-        self.timer = wx.Timer(self, self.onTimer)
-        self.timer.Start(500)
-        self.Bind(wx.EVT_TIMER, self.timer, self.onTimer)
         wx.Panel.__init__(self, parent, style=wx.WANTS_CHARS)
+        
+        self.timer = wx.Timer(self,  Board.TIMER_ID)
+        self.timer.Start(500)
+        self.Bind(wx.EVT_TIMER, self.onTimer, id=Board.TIMER_ID)
+        
+       # wx.Panel.__init__(self, parent, style=wx.WANTS_CHARS)
         self.contents = []
         self.Bind(wx.EVT_PAINT, self.drawObjects)
+        
+        self.Bind(wx.EVT_KEY_DOWN , self.onChar)
+        self.player = self.addObject(Moveable(10, 10, 10, 10, 'red'))
     def onTimer(self, event):
-        print "1"
+        #print "1"
+        nope=1
     def addObject(self, toAdd):
         self.contents.append(toAdd)
+        return self.contents[-1]
+    def onChar(self, event):
+        keycode = event.GetKeyCode()
+        self.player.move({ 83: (-5, 0)
+                          ,87: (5, 0)
+                          ,65: (0, -5)
+                          ,68: (0, 5)}[keycode])
+        if keycode in [83,wx.WXK_DOWN]:
+            self.player.move(5, 0)
+            print "Down"
+        if keycode in [87,wx.WXK_UP]:
+            print "Up"
+        if keycode in [65,wx.WXK_LEFT]:
+            print "Left"
+        if keycode in [68,wx.WXK_RIGHT]:
+            print "Right"
     def drawObjects(self, event):
         dc = wx.PaintDC(self)
-        for o in filter(lambda x: hasattr(x, "color"), self.contents):
-            o.draw(dc)
+        filtered = filter(lambda x: hasattr(x, "color"), self.contents)
+        for o in range(len(filtered)):
+            filtered[o].draw(dc)
+
+
+class Moveable(Drawable):
+    def __init__(self, x, y, width, height, color, borderColor = None, borderWidth = 2):
+        super(Moveable, self).__init__(x, y, width, height, color, [borderColor if borderColor else color, borderWidth])
+        self.velocity = [0, 0]
+    def move(self, move):
+        self.coords = map(sum, zip(move * 2, self.coords))
+
 
 class Obstacle(Drawable):
     def __init__(self, x, y, width, height, color):
-        super(Obstacle, self).__init__(x, y, width, height, color)
+        super(Obstacle, self).__init__(x, y, width, height, color, [color, 2])
 
 app = wx.App()
-Window(None, -1, 'Client')
+thingy = Window(None, -1, 'Client')
+w,h = thingy.GetSize()
+thingy.statusbar.SetStatusText(str(w))
+thingy.board.contents.append(Drawable(40,(h/2)-30,30,30,"#8F70FF", ["#DDDDDD", 2]))
+thingy.board.contents.append(Drawable(w-40-30,(h/2)-30,30,30,"#26FC14", ["#FFFFFF", 2]))
 app.MainLoop()
