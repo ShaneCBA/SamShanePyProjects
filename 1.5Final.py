db87446dbc99b0ff9c46afc0c9828626fbae4706
shanedrgn <shanedrgn@gmail.com>
2016-02-06 18:34:45 -0600

    Colorscheme and obstacles

--- 1.5Final.py
+++ 1.5Final.py
@@ -20,10 +20,10 @@ class Drawable(GameObject):
         pen = wx.Pen(self.border[0], self.border[1], wx.SOLID)
         dc.SetPen(pen)#.SetJoin(wx.JOIN_MITER))
         dc.SetBrush(wx.Brush(self.color, wx.SOLID))
-        dc.DrawRectangle(self.coords[0][0]+self.border[1], self.coords[0][1]+self.border[1], self.dimensions[0], self.dimensions[1])
+        dc.DrawRectangle(self.coords[0][0]+self.border[1], self.coords[0][1]+self.border[1], self.dimensions[0]-self.border[1], self.dimensions[1]-self.border[1])
 class Window(wx.Frame):
     def __init__(self, parent, id, title):
-        wx.Frame.__init__(self, parent, id, title, size=(1200, 600),  style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
+        wx.Frame.__init__(self, parent, id, title, size=(1200, 650),  style=wx.DEFAULT_FRAME_STYLE ^ wx.RESIZE_BORDER)
         self.statusbar = self.CreateStatusBar()
         self.statusbar.SetStatusText('0')
         self.board = Board(self)
@@ -40,15 +40,15 @@ class Board(wx.Panel):
         self.timer.Start(25)
         self.Bind(wx.EVT_TIMER, self.onTimer, id=Board.TIMER_ID)
         
-       # wx.Panel.__init__(self, parent, style=wx.WANTS_CHARS)
+       #wx.Panel.__init__(self, parent, style=wx.WANTS_CHARS)
         self.contents = []
         self.Bind(wx.EVT_PAINT, self.drawObjects)
+        self.SetBackgroundColour("#2A4152")#00F9FF
         
-        self.Bind(wx.EVT_KEY_DOWN , self.onChar)
         self.Bind(wx.EVT_KEY_DOWN , self.keyDown)
         self.Bind(wx.EVT_KEY_UP , self.keyUp)
         
-        self.player = self.addObject(Moveable(10, 10, 10, 10, 'red'))  
+        self.player = self.addObject(Moveable(0, 0, 30, 30, "#D9756D", "#DDDDDD", 2))  
     def onTimer(self, event):
         self.GetEventHandler().ProcessEvent(wx.PaintEvent( ))
         self.player.move()
@@ -70,7 +70,7 @@ class Board(wx.Panel):
         for o in range(len(filtered)):
             filtered[o].draw(dc)
 class Moveable(Drawable):
-    def __init__(self, x, y, width, height, color, borderColor = None, borderWidth = 2):
+    def __init__(self, x, y, width, height, color, borderColor = "#FFFFFF", borderWidth = 2):
         super(Moveable, self).__init__(x, y, width, height, color, [borderColor if borderColor else color, borderWidth])
         self.velocity = [1, 0]
     def move(self):
@@ -87,18 +87,17 @@ class Moveable(Drawable):
         if (velY < 0):
             self.velocity[1] +=1
             
-        if(keys[87]):
-            self.velocity[1] = -5#UP
+        if(keys[87]):#UP
+            self.velocity[1] = -10
             
-        if(keys[83]):
-            self.velocity[1] = 5#DOWN
+        if(keys[83]):#DOWN
+            self.velocity[1] = 10
         
-        if(keys[65]):
-            self.velocity[0] = -5#LEFT
+        if(keys[65]):#LEFT
+            self.velocity[0] = -10
             
-        if(keys[68]):
-            self.velocity[0] = 5#RIGHT
-            
+        if(keys[68]):#RIGHT
+            self.velocity[0] = 10
         
         x,y = self.coords[0]
         x2,y2 = self.coords[1]
@@ -109,14 +108,15 @@ class Moveable(Drawable):
         self.coords[1][1] = self.coords[1][1] + self.velocity[1]
         return 0
 class Obstacle(Drawable):
-    def __init__(self, x, y, width, height, color):
-        super(Obstacle, self).__init__(x, y, width, height, color, [color, 2])
+    def __init__(self, x, y, width, height, color, borderColor="#EEEEEE", borderWidth=2):
+        super(Obstacle, self).__init__(x, y, width, height, color, [borderColor, borderWidth])
 
 
 app = wx.App()
 thingy = Window(None, -1, 'Client')
 w,h = thingy.GetSize()
 thingy.statusbar.SetStatusText(str(w))
-thingy.board.contents.append(Drawable(40,(h/2)-30,30,30,"#8F70FF", ["#DDDDDD", 2]))
-thingy.board.contents.append(Drawable(w-40-30,(h/2)-30,30,30,"#26FC14", ["#FFFFFF", 2]))
+thingy.board.addObject(Obstacle(60,180,20,240,"#A13437"))
+thingy.board.addObject(Obstacle(1120,180,20,240,"#238C6F"))
+thingy.board.addObject(Moveable(1166,0,30,30,"#35D4A8"))
 app.MainLoop()
